60d9c975e713c8617cc591f524b19c218c496514
hulloCode <nilogm2012@gmail.com>
2024-06-11 00:17:04 -0300

    Upload classification notebooks

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# 2024-1-P1-Drug-Consumption
+# Drug Consumption Classifier
+
+Com este projeto, nós procuramos encontrar relações entre usuários de substâncias e algumas características pessoais e comportamentais.
+
+Este repositório contém o pré-processamento, as análises e alguns testes de classificadores no dataset.
+
+## Dataset
+
+O dataset utilizado neste projeto pode ser encontrado [aqui](https://www.kaggle.com/datasets/mexwell/drug-consumption-classification). O [notebook de pré-processamento](https://www.kaggle.com/code/mexwell/starter-notebook-convert-column-values) que foi utilizado como base para processar os dados originais também foi disponibilizado pelo autor original.
+
+# Executando os Experimentos
+
+Para executar os notebooks, é necessário instalar alguns pacotes:
+
+```bash
+pip install requirements.txt
+```
+
+## Pré-processamento
+
+Na pasta `data/`, existem duas versões do dataset: a original e a pré-processada (note que o pré-processamento não inclui a normalização dos dados).
+
+Dessa forma, o uso do notebook de pré-processamento não é necessário, mas serve para entender a nova organização do dataset.
+
+[descrever o notebook]
+
+## Classificação
+
+Nesse notebook, observamos várias facetas do problema, tentando encontrar padrões que possam ajudar a obter uma interpretação melhor dos dados.
+
+De início, utilizamos da biblioteca `LazyClassifier` para medir a capacidade de predição de vários classificadores em substâncias relevantes. Dessa forma, podemos filtrar um top $k$ de classificadores e testá-los corretamente na base de dados.
+
+Subsequentemente, são exibidas tabelas e testes que determinam a performance dos classificadores, entretanto, sem muito aprofundamento nas nuâncias do dataset.
+
+### Classificação +
+
+Para continuar investigando possíveis combinações, um notebook extra examina vertentes do problema por aplicar mais configurações aos procedimentos. Alguns dos testes incluem:
+ 
+ - Filtro de características,
+ - Seleção de samples,
+ - Aplicação de redução de dimensionalidade,
+ - Classificadores multiclasse e
+ - Classificadores multilabel.
+
+Com esses experimentos, procuramos encontrar padrões que possam estar menos reconhecíveis por conta da distribuição dos dados.
---
+++ utils.py
@@ -0,0 +1,236 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import StratifiedKFold
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.semi_supervised import LabelPropagation, LabelSpreading
+from sklearn.discriminant_analysis import (
+    QuadraticDiscriminantAnalysis,
+    LinearDiscriminantAnalysis,
+)
+from sklearn.calibration import CalibratedClassifierCV
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+
+numCols = [
+    "Neuroticism",
+    "Extraversion",
+    "Openness",
+    "Agreeableness",
+    "Conscientiousness",
+    "Impulsiveness",
+    "Sensationness",
+]
+
+drugs = [
+    "Alcohol",
+    "Amphet",
+    "Amyl",
+    "Benzos",
+    "Caff",
+    "Cannabis",
+    "Choc",
+    "Coke",
+    "Crack",
+    "Ecstasy",
+    "Heroin",
+    "Ketamine",
+    "Legalh",
+    "LSD",
+    "Meth",
+    "Mushrooms",
+    "Nicotine",
+    # "Semer",
+    "VSA",
+]
+
+# Substâncias com menor relevância de análise.
+lesser_drugs = ["Choc", "Alcohol", "Caff"]
+
+classifiers = [
+    NearestCentroid(),
+    GaussianNB(),
+    BernoulliNB(),
+    QuadraticDiscriminantAnalysis(),
+    CalibratedClassifierCV(),
+    AdaBoostClassifier(),
+    LinearDiscriminantAnalysis(),
+    LabelPropagation(),
+    LabelSpreading(),
+]
+
+order_ = {
+    0: "Never Used",
+    1: "Used over a Decade Ago",
+    2: "Used in Last Decade",
+    3: "Used in Last Year",
+    4: "Used in Last Month",
+    5: "Used in Last Week",
+    6: "Used in Last Day",
+}
+
+
+def metric_preprocessor():
+    return ColumnTransformer(
+        [("numerical", StandardScaler(), numCols)],
+        verbose_feature_names_out=False,
+        remainder="passthrough",
+    )
+
+
+def test_classifiers(
+    X: pd.DataFrame,
+    labels: pd.DataFrame,
+    classifiers: list = classifiers,
+    pipe: tuple = (metric_preprocessor(),),
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Realiza testes de validação cruzada para cada classificador e substância dada.
+
+    Args:
+        X (pd.DataFrame):.
+        labels (pd.DataFrame): todos os labels de substâncias.
+        pipe (tuple, optional): pipeline de transformação de dados. Defaults to (metric_preprocessor(),).
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: _description_
+    """
+    all_results = {}
+    all_df = pd.DataFrame(columns=drugs)
+    conf_df = None
+
+    n_classes = len(np.unique(labels))
+    conf_df = pd.DataFrame(
+        columns=["Model", "Substância"]
+        + [f"cm_{i}" for i in range(n_classes * n_classes)]
+    )
+
+    for c in classifiers:
+        name = c.__class__.__name__
+        for d in drugs:
+            pipe_ = make_pipeline(*pipe, c)
+
+            y = labels[d].to_numpy()
+            results = []
+
+            splitter = StratifiedKFold(8)
+            for train, test in splitter.split(X, y):
+                # fit & predict
+                pipe_.fit(X.iloc[train], y[train])
+                preds = pipe_.predict(X.iloc[test])
+
+                # append score
+                results.append(balanced_accuracy_score(y[test], preds))
+
+                # draw confusion matrix
+                cm = confusion_matrix(y[test], preds)
+                conf_df.loc[len(conf_df)] = [name, d] + [y for x in cm for y in x]
+
+            all_results[d] = results
+
+        df_ = pd.DataFrame(all_results)
+        df_["Model"] = name
+        all_df = pd.concat([all_df, df_], ignore_index=True)
+
+    return all_df.melt("Model", var_name="Substância", value_name="Score"), conf_df
+
+
+def boxplot(df: pd.DataFrame, title="", lims=(0.4, 1), refs=[0.5, 0.8]) -> None:
+    """Plota o boxplot de pontuações para cada substância e os modelos.
+
+    Args:
+        df (pd.DataFrame): dataframe de pontuações com "Model", "Substância" e "Score".
+        title (str, optional): título do boxplot. Defaults to "".
+        lims (tuple, optional): limites da figura. Defaults to (0.4, 1).
+        refs (list, optional): linhas para referência de pontuações. Defaults to [0.5, 0.8].
+    """
+    _, ax = plt.subplots(1, 1, figsize=(22, 6), sharex=True, sharey=True)
+
+    if lims is not None:
+        ax.set_ylim(*lims)
+
+    if refs is not None:
+        for i in refs:
+            plt.axhline(i, linestyle="--")
+
+    plt.title(title)
+    sns.boxplot(data=df, x="Substância", y="Score", hue="Model", ax=ax)
+
+
+def confusion(cm: pd.DataFrame) -> None:
+    """Plota a matriz de confusão dado um dataframe com colunas "Model", "Substância" e colunas da matriz no formato "cm_i".
+
+    Args:
+        cm (pd.DataFrame): dataframe de matriz de confusão.
+    """
+    cms = cm.groupby(by=["Model", "Substância"], as_index=False).sum()
+
+    models = np.unique(cms.Model)
+    subs = np.unique(cms["Substância"])
+    mat_cols = [c for c in cm.columns if c.startswith("cm_")]
+    size = int(math.sqrt(len(mat_cols)))
+
+    for m in models:
+        fig, ax = plt.subplots(3, 6, figsize=(14, 6))
+        fig.suptitle(m)
+        for a, s in zip(ax.flatten(), subs):
+            c = cms.loc[(cms["Model"] == m) & (cms["Substância"] == s), mat_cols]
+            a.set_title(s)
+            sns.heatmap(
+                np.reshape(c.to_numpy(), (size, size)),
+                ax=a,
+                annot=True,
+                fmt=".10g",
+                cmap="viridis",
+            )
+
+
+def show_statistics(df: pd.DataFrame) -> pd.DataFrame:
+    """Reformata o DataFrame para um formato mais legível.
+
+    Args:
+        df (pd.DataFrame): dataframe com modelos, substâncias e pontuações.
+
+    Returns:
+        pd.DataFrame: dataframe formatado com substâncias como colunas e (média, std) como subcolunas.
+    """
+    # Agrupa por classificador e substância e tira média e desvio padrão.
+    g = df.groupby(by=["Model", "Substância"], as_index=False).agg(
+        {"Score": ["mean", "std"]}
+    )
+
+    # Inverte a tabela de forma que as substância virem colunas
+    g = g.pivot_table(
+        values="Score", index="Model", columns="Substância", aggfunc="first"
+    )
+
+    # Coloca importância nas substâncias
+    g = g.swaplevel(0, 1, axis=1)
+
+    # Ordena para que média e desvio padrão fiquem lado a lado.
+    g = g.sort_index(level=0, axis=1)
+
+    return g
+
+
+def threshold(x: int, t: list[int] = [2]) -> int:
+    """Realiza a re-rotulação do label de acordo com o threshold dado. O threshold é feito com "<", ou seja, não inclusivo.
+
+    Args:
+        x (int): label
+        t (list[int], optional): valores de threshold para cada classe (em ordem). Defaults to [2].
+
+    Returns:
+        int: rótulo novo.
+    """
+    for i, j in enumerate(t):
+        if x < j:
+            return i
+    return i + 1
